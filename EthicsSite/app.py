086c69f06c6b17967391fe6ba3189c779f0a6af5
Matthew <matthewaj13@gmail.com>
2019-02-17 20:57:59 -0600

    Update routing for index page to render home template

--- EthicsSite/app.py
+++ EthicsSite/app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index_page():
-    return 'Hello World!'
+    return render_template('index_template.html', name='index')
 
 
 if __name__ == '__main__':
