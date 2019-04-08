adc5799996cfaab4ab0c988511a3e3ddc34a9714
Matthew <matthewaj13@gmail.com>
2019-04-07 23:24:32 -0500

    Update app routing
    Changed from single quotes to double quotes for consistency.
    The variable name was also removed from the template, index, since it
    was unused. Lastly, the routing was added for the games page, "/game".

--- EthicsSite/app.py
+++ EthicsSite/app.py
@@ -3,10 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index_page():
-    return render_template('index_template.html', name='index')
+    return render_template("index_template.html")
 
+
+@app.route("/game")
+def game_page():
+    return render_template("game.html", title="Ethics Game")
 
 if __name__ == '__main__':
     app.run()
