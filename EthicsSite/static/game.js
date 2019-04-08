aa54deb17d440f90302f018c347d41e0e07fe60a
Matthew <matthewaj13@gmail.com>
2019-04-07 23:18:55 -0500

    Create game.js to provide an interactive quiz on /game page
    Added the function, startGame, to remove the initial page text and
    display the actual quiz.

---
+++ EthicsSite/static/game.js
@@ -0,0 +1,8 @@
+function startGame() {
+    let main_screen = document.getElementById("main_text");
+    let game = document.getElementById("game");
+    main_screen.style.display = "none";
+
+    game.style.display = "block";
+}
+
